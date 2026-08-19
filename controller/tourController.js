8de7f8b81baa3322b9e43e4cab9be6b9434e005f
Jiya Sinha <jiya15092007@gmail.com>
2026-08-19 14:17:07 +0530

    initial commit

--- controller/tourController.js
+++ controller/tourController.js
@@ -5,16 +5,6 @@ const getAllTours = (req, res) => {
     res.json(tours);
 };
 
-// Get a tour by Id
-const getTourById = (req, res) => {
-    const id = parseInt(req.params.id);
-    if (tour) {
-        res.json(tour);
-    } else {
-        res.status(404).json({ message: 'Tour not found' });
-    }
-};
-
 module.exports = {
     getAllTours,
     getTourById
